a9a873ad9ff28f8977f440965e81db3819f87d81
FlinT <User@FlinT-.(none)>
2013-08-03 21:17:11 +0300

    add rocket

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 jQuery.noConflict();
 jQuery(document).ready(function ($) {
-	
+	$('body').keydown(function(shotClick){
+	  if(shotClick.which == 32){
+		var leftShotPosition = parseInt($('.tank').css('left')) + ($('.tank').width() / 2);
+		$('#wrapper').append('<div class="rocket" />');
+		
+		function removeRocket(){
+			$(this).remove();
+		}
+		$('.rocket').css('left',leftShotPosition).animate({bottom: '500px'},1000, removeRocket);	
+	  }
+	});
 });
