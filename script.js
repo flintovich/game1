9cd5fa0bc2c01f44cdc3eecd01276e790563a719
flintovich <flintovich@gmail.com>
2013-09-01 18:01:59 +0200

    some fix

--- script.js
+++ script.js
@@ -1,98 +1,95 @@
 jQuery.noConflict();
 jQuery(document).ready(function ($) {
-	
-	$(document).keydown(function(keyclick){
-		var leftpositionStyle = $('.tank').css('left');
-		var leftposition = parseInt(leftpositionStyle);
-	  	if(keyclick.which == 39) {
-			var i = leftposition;
-				if(i == 520){
-					return false
-				}
-				var i = i + 15;
-				$('.tank').stop().animate({left: i}, 5, 'swing');
 
-		} else if (keyclick.which == 37){
-			var i = leftposition;
-			if(i == -5){
-				return false
-			}
-			var i = i - 15;
-			$('.tank').stop().animate({left: i}, 5, 'swing');
+    $(document).keydown(function(keyclick){
+        var leftpositionStyle = $('.tank').css('left');
+        var leftposition = parseInt(leftpositionStyle);
+        if(keyclick.which == 39) {
+            var i = leftposition;
+            if(i == 520){
+                return false
+            }
+            var i = i + 15;
+            $('.tank').stop().animate({left: i}, 5, 'swing');
 
-		} else if (keyclick.which == 32) {
-			var rocket = true;
+        } else if (keyclick.which == 37){
+            var i = leftposition;
+            if(i == -5){
+                return false
+            }
+            var i = i - 15;
+            $('.tank').stop().animate({left: i}, 5, 'swing');
 
-			var leftShotPosition = parseInt($('.tank').css('left')) + ($('.tank').width() / 2);
-			$('#wrapper').append('<div class="rocket" />');
+        } else if (keyclick.which == 32) {
+            var rocket = true;
 
-			function removeRocket(){
-				rocket = false;
-				$(this).remove();
-			}
-			$('.rocket').css('left',leftShotPosition).animate({bottom: '700px'},800, 'linear', removeRocket);
-			function count(){
-				if(rocket == true){
-					$(".bug").each(function(){
-						// bugs coordinates
-						var topBug = $(this).position().top;
-						var bottomBug = topBug + $(this).height();
-						var leftBug = $(this).position().left;
-						var rightBug = $(this).position().left + $(this).width();
+            var leftShotPosition = parseInt($('.tank').css('left')) + ($('.tank').width() / 2);
+            $('#wrapper').append('<div class="rocket" />');
 
-						// rocket coordinates
-						var topRocket = $('.rocket').position().top;
-						var leftRocket = $('.rocket').position().left;
+            function removeRocket(){
+                rocket = false;
+                $(this).remove();
+            }
+            $('.rocket').css('left',leftShotPosition).animate({bottom: '700px'},800, 'linear', removeRocket);
+            function count(){
+                if(rocket == true){
+                    $(".bug").each(function(){
+                        // bugs coordinates
+                        var topBug = $(this).position().top;
+                        var bottomBug = topBug + $(this).height();
+                        var leftBug = $(this).position().left;
+                        var rightBug = $(this).position().left + $(this).width();
 
-						if(topRocket > topBug && topRocket < bottomBug && leftRocket > leftBug && leftRocket < rightBug){
-							rocket = false;
-							$(this).remove();
-							$('.rocket').eq(0).remove();
-						}
-					});
-				}
-			}
-			setInterval(count, 30);
+                        // rocket coordinates
+                        var topRocket = $('.rocket').position().top;
+                        var leftRocket = $('.rocket').position().left;
 
-			return false
-		}
-	});
-	
-	// roud
-	var startRoud = -9200;
-	function changeRoad(){
-		if(startRoud >= 10){
-			startRoud = -9200;
-		}
-		$('#wrapper').css('backgroundPosition','0' +startRoud+'px');
-		startRoud = startRoud + 3;
+                        if(topRocket > topBug && topRocket < bottomBug && leftRocket > leftBug && leftRocket < rightBug){
+                            rocket = false;
+                            $(this).remove();
+                            $('.rocket').eq(0).remove();
+                        }
+                    });
+                }
+            }
+            setInterval(count, 30);
+
+            return false
+        }
+    });
+
+    // roud
+    var startRoud = -9200;
+    function changeRoad(){
+        if(startRoud >= 10){
+            startRoud = -9200;
+        }
+        $('#wrapper').css('backgroundPosition','0' +startRoud+'px');
+        startRoud = startRoud + 3;
 
         $('.bug').each(function(){
             if($(this).offset().top >= '770'){
                 alert('Проиграл!');
             }
         });
-	}
-	setInterval(changeRoad, 10);
-	
-	// add rand bug
-	function addBug(){
-		function rand (min, max){
-		  min = parseInt(min);
-		  max = parseInt(max);
-		  return Math.floor( Math.random() * (max - min + 1)) + min;
-		}
-		randNubrer = rand(1, 540);
-		
-		function removeBug(){
-			$(this).remove();
-		}
-		$('#wrapper').append('<div style="left: '+randNubrer+'px" class="bug" />');
-		$('.bug').animate({top: '700px'}, 4500, 'linear', removeBug);
-	}
-	setInterval(addBug, 3200);
-	
-});
+    }
+    setInterval(changeRoad, 10);
 
- 
-	
+    // add rand bug
+    function addBug(){
+        function rand (min, max){
+            min = parseInt(min);
+            max = parseInt(max);
+            return Math.floor( Math.random() * (max - min + 1)) + min;
+        }
+        randNubrer = rand(1, 540);
+
+        function removeBug(){
+            $(this).remove();
+        }
+        $('#wrapper').append('<div style="left: '+randNubrer+'px" class="bug" />');
+        $('.bug').animate({top: '700px'}, 4500, 'linear', removeBug);
+    }
+    setInterval(addBug, 3200);
+
+});
